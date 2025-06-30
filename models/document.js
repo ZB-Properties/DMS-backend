1bc4ed7e5a783c7efc223ff8be2b69575d728b72
Shope Alli-B <zoeyalli921@gmail.com>
2025-06-30 04:18:56 -0700

    dms backend

--- models/document.js
+++ models/document.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 const documentSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    filename: String,
-    originalName: String,
-    fileType: String,
-    mimetype: String,
-    size: Number,
-    path: String,
-    text: String,
-    uploadDate: { type: Date, default: Date.now }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  originalName: String,
+  fileType: String,
+  mimetype: String,
+  size: Number,
+  cloudinaryUrl: String,           
+  cloudinaryPublicId: String,      
+  text: String,
+  uploadDate: { type: Date, default: Date.now }
 });
+
 
 documentSchema.index({ text: 'text', originalName: 'text' });
 
